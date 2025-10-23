fa4a7e838f79546dbd70837c770dcc0dc49ba6af
“jtai56” <joshtai21@gmail.com>
2025-10-22 21:45:32 -0600

    implemented login

--- src/app.jsx
+++ src/app.jsx
@@ -8,39 +8,64 @@ import { Log } from './log/log';
 import { Leaderboard } from './leaderboard/leaderboard';
 import { About } from './about/about';
 
+const AuthState = {
+  Authenticated: 'Authenticated',
+  Unauthenticated: 'Unauthenticated'
+};
+
 export default function App() {
+  const [user, setUser] = React.useState(localStorage.getItem('user') || null);
+  const [userName, setUserName] = React.useState(user || '');
+  const [authState, setAuthState] = React.useState(user ? AuthState.Authenticated : AuthState.Unauthenticated);
+
   return (
     <BrowserRouter>
-        <div className="body bg-dark">
-            <header className="header">
-                <h1> Level Up</h1>
-                <nav className = "navbar">
-                    <ul>
-                        <li><NavLink to='/'>Login</NavLink></li>
-                        <li><NavLink to='/about'>About</NavLink></li>
-                        <li><NavLink to='/log'>Log</NavLink></li>
-                        <li><NavLink to='/leaderboard'>Leaderboard</NavLink></li>
-                    </ul>
-                </nav>
-            </header>
-            
-            <Routes>
-                <Route path='/' element={<Login />} exact />
-                <Route path='/log' element={<Log />} />
-                <Route path='/leaderboard' element={<Leaderboard />} />
-                <Route path='/about' element={<About />} />
-                <Route path='*' element={<NotFound />} />
-            </Routes>
+      <div className="body bg-dark">
+        <header className="header">
+          <h1>Level Up</h1>
+          <nav className="navbar">
+            <ul>
+              <li><NavLink to='/'>Login</NavLink></li>
+              <li><NavLink to='/about'>About</NavLink></li>
+              {authState === AuthState.Authenticated && (
+                <>
+                  <li className='nav-item'><NavLink className='nav-link' to='/log'>Log</NavLink></li>
+                  <li className='nav-item'><NavLink className='nav-link' to='/leaderboard'>Leaderboard</NavLink></li>
+                </>
+              )}
+            </ul>
+          </nav>
+        </header>
 
-            <footer>    
-                <div>
-                    <span>Joshua Tai</span>
-                    <a href="https://github.com/jtai56/startup">My Github</a>
-                </div>
-            </footer>
-        </div>
+        <Routes>
+          <Route
+            path='/'
+            element={
+              <Login
+                userName={userName}
+                authState={authState}
+                onAuthChange={(userName, authState) => {
+                  setAuthState(authState);
+                  setUserName(userName);
+                }}
+              />
+            }
+          />
+          <Route path='/log' element={<Log />} />
+          <Route path='/leaderboard' element={<Leaderboard />} />
+          <Route path='/about' element={<About />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+
+        <footer>
+          <div>
+            <span>{userName}</span>
+            <a href="https://github.com/jtai56/startup">My Github</a>
+          </div>
+        </footer>
+      </div>
     </BrowserRouter>
-    );
+  );
 }
 
 function NotFound() {
--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,27 +1,48 @@
 import React from 'react';
 
-export function Login() {
+export function Login({ userName, authState, onAuthChange }) {
+  const [loginName, setLoginName] = React.useState('');
+
+  function loginUser(e) {
+    e.preventDefault(); // prevents page reload
+    if (!loginName.trim()) return;
+    localStorage.setItem('user', loginName);
+    onAuthChange(loginName, 'Authenticated'); 
+  }
+
+  function textChange(e) {
+    setLoginName(e.target.value);
+  }
+
   return (
-    <main className = 'main'>
-        <container>
-            <h1>Lets repo your progress.</h1>
-            <h1>Github but for pushing your life forward.</h1>
-            <form method="get" action="play.html">
-                <div className="login">
-                    <div>
-                    <span>👤 </span><input type="text" placeholder="your@email.com" />
-                    </div>
-                    <div>
-                    <span>🔒</span>
-                    <input type="password" placeholder="password" />
-                    </div>
-                </div>
-                <div className="loginButtons"> 
-                    <button type="submit">Login</button>
-                    <button type="submit">Create</button>
-                </div>
-            </form>
-        </container>
+    <main className='main'>
+      <div className="container">
+        <h1>Let's repo your progress.</h1>
+        <h2>Github but for pushing your life forward.</h2>
+        <form onSubmit={loginUser}>
+          <div className="login">
+            <div className="inputs">
+              <div>
+                <span>👤 </span>
+                <input
+                  type="text"
+                  placeholder="your@email.com"
+                  value={loginName}
+                  onChange={textChange}
+                />
+              </div>
+              <div>
+                <span>🔒</span>
+                <input type="password" placeholder="password" />
+              </div>
+            </div>
+          </div>
+          <div className="loginButtons">
+            <button type="submit">Login</button>
+            <button type="button">Create</button>
+          </div>
+        </form>
+      </div>
     </main>
   );
 }
